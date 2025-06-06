c2ea2834684ec66cefb2393586b9726d8be60afe
Xcilik <149920327+Xcilik@users.noreply.github.com>
2025-06-06 19:59:02 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv(".env")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7401820056:AAHU9cijeViZ1g9-9TXqzlZ7lsbXWf9hDnw")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7521309897:AAEITsse9EQY_BvJteguTl3GDzEroKbsNVQ")
 API_ID = int(os.environ.get("API_ID", "21445722"))
 API_HASH = os.environ.get("API_HASH", "710f18f90849255dd85837d00d5fe85f")
 OWNER = int(os.environ.get("OWNER", "1784606556"))
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://chat:chat@cluster0.jt616lp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://chatbot:chatbot@cluster0.wvgny.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 MUST_JOIN = os.environ.get("MUST_JOIN", "katalogbottelegram")
-SELLER_GROUP = int(os.environ.get("SELLER_GROUP", "-1002091170438"))
