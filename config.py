2b41a81dd9fba6d39573262ea5d30762af7a567f
Xcilik <149920327+Xcilik@users.noreply.github.com>
2025-06-06 20:05:33 +0700

    Update config.py

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ API_HASH = os.environ.get("API_HASH", "710f18f90849255dd85837d00d5fe85f")
 OWNER = int(os.environ.get("OWNER", "1784606556"))
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://chatbot:chatbot@cluster0.wvgny.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 MUST_JOIN = os.environ.get("MUST_JOIN", "katalogbottelegram")
-SELLER_GROUP = int(os.environ.get("SELLER_GROUP", "-1002091170438"))
+SELLER_GROUP = int(os.environ.get("SELLER_GROUP", "-1001917973794"))
 
